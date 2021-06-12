534fe856d1eda48ba3c50144504fc0babb07e674
Variiiest <varishgautamvari@gmail.com>
2021-06-12 17:51:05 +0530

    Service worker removed due to build error

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
-import * as serviceWorker from './serviceWorker';
-
 
 
 axios.defaults.headers = {
@@ -26,4 +24,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
+
